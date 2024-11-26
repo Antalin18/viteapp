27af05744ed2826b13b4aa7b4130229484d0840b
Your Name <antalinpricy07@gmail.com>
2024-11-26 02:01:21 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <h1>App Component</h1>
+      <Footer/>
+      </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        <h1>Header</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Amet nam aliquid voluptas pariatur illo, numquam omnis saepe vero voluptatum laudantium
+        dignissimos necessitatibus soluta vel reiciendis quasi quaerat nesciunt repellendus. Eos.</p>
+    </div>
+  )
+}
+
+export default Header
